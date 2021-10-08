9e923c65076b358a95faf9af688c789d6d65af03
puyuelin <1427977859@qq.com>
2021-10-08 14:43:22 +0800

    修改

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-
-import "./index.less"
+import ProductHome from "./ProductHome";
+import ProductAddUpdate from "./ProductAddUpdate";
+import ProductDetail from "./ProductDetail";
+import "./index.less";
 
 export default class Product extends Component {
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div>index</div>;
   }
 }
