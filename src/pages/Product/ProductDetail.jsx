235991476dd34b18d212db306b0fcb5867e2b422
huanghl <1427977859@qq.com>
2021-10-08 23:08:09 +0800

    修改

--- src/pages/Product/ProductDetail.jsx
+++ src/pages/Product/ProductDetail.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { List, Card } from "antd";
+import { ArrowUpOutlined } from "@ant-design/icons";
 
-import LinkButton from "../../components/link-button";
+import LinkButton from "../../components/LinkButton";
 import { BASE_IMG_URL } from "../../utils/constants";
 import { reqCategory } from "../../api";
 
@@ -12,6 +13,61 @@ export default class ProductDetail extends Component {
   };
 
   render() {
-    return <div>ProductDetail</div>;
+    // 读取携带过来的state数据
+    const { name, desc, price, detail, imgs } =
+      this.props.location.state.product;
+    const { cName1, cName2 } = this.state;
+
+    const title = (
+      <span>
+        <LinkButton onClick={() => this.props.history.goBack()}>
+          <ArrowUpOutlined style={{ marginRight: 10, fontSize: 20 }} />
+        </LinkButton>
+
+        <span>商品详情</span>
+      </span>
+    );
+
+    return (
+      <Card title={title} className="product-detail">
+        <List>
+          <List.Item>
+            <span className="left">商品名称:</span>
+            <span>{name}</span>
+          </List.Item>
+          <List.Item>
+            <span className="left">商品描述:</span>
+            <span>{desc}</span>
+          </List.Item>
+          <List.Item>
+            <span className="left">商品价格:</span>
+            <span>{price}元</span>
+          </List.Item>
+          <List.Item>
+            <span className="left">所属分类:</span>
+            <span>
+              {cName1} {cName2 ? " --> " + cName2 : ""}
+            </span>
+          </List.Item>
+          <List.Item>
+            <span className="left">商品图片:</span>
+            <span>
+              {imgs.map((img) => (
+                <img
+                  key={img}
+                  src={BASE_IMG_URL + img}
+                  className="product-img"
+                  alt="img"
+                />
+              ))}
+            </span>
+          </List.Item>
+          <List.Item>
+            <span className="left">商品详情:</span>
+            <span dangerouslySetInnerHTML={{ __html: detail }}></span>
+          </List.Item>
+        </List>
+      </Card>
+    );
   }
 }
